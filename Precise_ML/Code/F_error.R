ab4a02c037d352e4054e341ff928a5e00b5dcfb9
Axect <edeftg@gmail.com>
2018-06-25 21:16:56 +0900

    F-Error R Plot

---
+++ Precise_ML/Code/F_error.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+
+bayes <- function(x) min(x,1-x)
+nearest <- function(x) 2*x*(1-x)
+matushita <- function(x) sqrt(x*(1-x))
+entropy <- function(x) -x*log(x)-(1-x)*log(1-x)
+
+x <- seq(0.001,0.999, 0.01)
+
+bV <- Vectorize(bayes)
+nV <- Vectorize(nearest)
+mV <- Vectorize(matushita)
+eV <- Vectorize(entropy)
+
+lab <- c(rep("bayes",100),rep("nearest", 100),rep("matushita",100),rep("entropy",100))
+val <- c(bV(x), nV(x), mV(x), eV(x))
+
+df = data.frame(x, val, lab)
+
+png("F-error.png", units="in", width=8, height=6, res=600)
+ggplot(data=df, aes(x=x,y=val,group=lab, colour=lab)) + geom_line()
+dev.off()
