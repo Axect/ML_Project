d8e8b866bda07adf228fbfff393d6fe6b3f09dd6
Axect <edeftg@gmail.com>
2018-07-14 02:45:39 +0900

    Runif

--- Lecture/2nd/Code/HW/source/app.d
+++ Lecture/2nd/Code/HW/source/app.d
@@ -1,7 +1,26 @@
 import std.stdio;
+import std.random : uniform;
 import dnum.vector;
 
 void main() {
   auto a = Vector(1,10);
   a.writeln;
+  auto b = runif(5, -1, 1);
+  b.writeln;
+}
+
+// Matrix weightsInit(int m, int n) {
+  
+// }
+
+Vector runif(int n, double a, double b) {
+  import std.random : Random, unpredictableSeed;
+  import std.array : array;
+  import std.range : generate, takeExactly;
+
+  auto rnd = Random(unpredictableSeed);
+
+  double[] w = generate!(() => uniform(a, b)).takeExactly(n).array;
+
+  return Vector(w);
 }
