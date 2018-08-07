c694945f2b8acfb8c4332c4e351b7e14e0ad7c6c
Axect <edeftg@gmail.com>
2018-08-07 14:54:46 +0900

    Midterm Stats

---
+++ Lecture/Midterm/Stat/stat.R
@@ -0,0 +1,34 @@
+weight <- c(64,66,68,69,73)
+score <- c(580,570,590,660,600)
+age <- c(39,33,37,46,55)
+
+m <- cbind(weight, score, age)
+
+covmat <- function(cols) {
+  mat = matrix(0, ncol(cols), ncol(cols))
+  for (i in 1:ncol(cols)) {
+    for (j in 1:ncol(cols)) {
+      mat[i,j] = cov(cols[,i], cols[,j])
+    }
+  }
+  return(mat)
+}
+
+mahal <- function(p, dat) {
+  ms <- colMeans(dat)
+  cs <- covmat(dat)
+  return(sqrt(t(p - ms) %*% solve(cs) %*% (p - ms)))
+}
+
+# 1. Find covariance matrix
+covm <- covmat(m)
+print(covm)
+
+# 2. Correlation
+rho <- cor(weight, score)
+print(rho)
+
+# 3. Person
+dat <- c(66, 640, 44)
+md <- mahal(dat, m)
+print(md)
