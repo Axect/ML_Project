f73ee9349b2c7feb627bc762f36a5cd3be64dc32
axect <edeftg@gmail.com>
2018-11-03 16:01:54 +0900

    Benchmark

---
+++ Lecture/3rd/Code/plot.R
@@ -0,0 +1,11 @@
+library("ggpubr")
+
+dat <- read.csv("bench.csv")
+
+Library <- c("DNumeric1(D)", "DNumeric2(D)", "DNumeric3(D)", "R", "Peroxide(Rust)")
+
+df <- cbind(dat, Library)
+
+g <- ggline(df, "Library", "mean", type='l', ylab="time(s)", title="Benchmark for XOR MLP")
+g 
+#+ yscale("log10", .format=TRUE)
